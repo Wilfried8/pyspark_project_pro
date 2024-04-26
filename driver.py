c6d1f8260ee4922be6be3bc2836c1d528398f428
wilfried TAYO <wktayo@gmail.com>
2024-04-27 00:58:58 +0200

    ingest data

--- driver.py
+++ driver.py
@@ -1,8 +1,10 @@
+import os
 import sys
 
 import get_env_variables as gev
 from create_spark import get_spark_objet
 from validate import get_current_date
+from ingest_data import load_files, display_data, df_count
 import logging
 import logging.config
 
@@ -20,6 +22,56 @@ def main():
 
         logging.info('validation spark object ....')
         get_current_date(spark)
+
+        for file in os.listdir(gev.source_olap):
+            logging.info('files are ' + file)
+            file_direction = gev.source_olap + '/' + file
+            logging.info(file_direction)
+
+            if file.endswith('.parquet'):
+                file_format = 'parquet'
+                header = 'NA'
+                inferSchema = 'NA'
+
+            elif file.endswith('.csv'):
+                file_format = 'csv'
+                header = gev.header
+                inferSchema = gev.inferSchema
+
+        logging.info('we are reading a file with format {}'.format(file_format))
+
+        df_city = load_files(spark=spark, file_format=file_format, file_direction=file_direction, header=header,
+                             inferSchema=inferSchema)
+        logging.info('displaying the dataframe {}'.format(df_city))
+        display_data(df=df_city, dfname='df_cty')
+
+        logging.info('validating the dataframe ..... ')
+        df_count(df=df_city, dfname='df_city')
+
+        for file2 in os.listdir(gev.source_oltp):
+            logging.info('files are ' + file2)
+            file_direction = gev.source_oltp + '/' + file2
+            logging.info(file_direction)
+
+            if file2.endswith('.parquet'):
+                file_format = 'parquet'
+                header = 'NA'
+                inferSchema = 'NA'
+
+            elif file2.endswith('.csv'):
+                file_format = 'csv'
+                header = gev.header
+                inferSchema = gev.inferSchema
+
+        logging.info('we are reading a file with format {}'.format(file_format))
+
+        df_medicare = load_files(spark=spark, file_format=file_format, file_direction=file_direction, header=header,
+                             inferSchema=inferSchema)
+        logging.info('displaying the dataframe {}'.format(df_medicare))
+        display_data(df=df_city, dfname='df_medicare')
+
+        logging.info('validating the dataframe ..... ')
+        df_count(df=df_medicare, dfname='df_medicare')
 
     except Exception as e:
         logging.info(f"we have an error with a exception : {str(e)}")
---
+++ ingest_data.py
@@ -0,0 +1,51 @@
+import logging.config
+
+logging.config.fileConfig('Properties/configuration/logging.conf')
+loggers = logging.getLogger('ingest_data')
+
+
+def load_files(spark, file_format, file_direction, header, inferSchema):
+    loggers.info("load files method started ")
+
+    try:
+        if file_format == 'parquet':
+            df = spark.read.format(file_format).load(file_direction)
+        elif file_format == 'csv':
+            # df = spark.read.format(file_format).option(header=header).option(inferSchema=inferSchema) \
+            #   .load(file_direction)
+            df = spark.read.option("header", header).csv(file_direction)
+
+    except Exception as e:
+        loggers.error(f"we have an error with a exception : {str(e)}")
+        raise
+    else:
+        loggers.info("dataframe created successfully , Good Job, with format {} ".format(file_format))
+    return df
+
+
+def display_data(df, dfname):
+    try:
+        loggers.info('Here we display the top 8 in the {}'.format(dfname))
+        df_show = df.show(8)
+
+    except Exception as e:
+        loggers.error(f"we have an error with a exception : {str(e)}")
+        raise
+    else:
+        loggers.info("dataframe displayed successful , Good Job, with name {} ".format(dfname))
+    return df_show
+
+
+def df_count(df, dfname):
+    try:
+        loggers.info('Here to count the records in the {}'.format(dfname))
+        df_c = df.count()
+
+    except Exception as e:
+        loggers.error(f"we have an error with a exception : {str(e)}")
+        raise
+    else:
+        loggers.info("Number of records present in the {} are {} ".format(df, df_c))
+    return df_c
+
+
